3f8d44834b5ecdc7adbc4a86276c99e063b7d79a
thagberg <timothy.m.hagberg@gmail.com>
2018-09-11 13:04:59 -0400

    Simple crash tester

---
+++ crashtest.py
@@ -0,0 +1,29 @@
+import subprocess
+import argparse
+
+from tester import Tester
+
+commands = [
+    ['ls', '-l'],
+    ['exit 1'],
+    ['exit 0']
+]
+# commands = [
+#     ['rm -rf ../appData/cache/'],
+#     ['rm -rf ./appData/cache/'],
+#     ['./vrvStealth.exe --showConsole --plugin vrvAutoTestPlugin.dll -e "SavedViewTest()" "../userData/terrains/Ala Moana.mtf" -n 3']
+# ]
+
+parser = argparse.ArgumentParser(description="Run commands and log any crashes")
+parser.add_argument('--loop', type=bool, action='store', default=False)
+args = parser.parse_args()
+
+def main():
+    tester = Tester(commands)
+    should_run = True
+    while should_run:
+        tester.run()
+        should_run = args.loop
+
+if __name__ == '__main__':
+    main()
---
+++ tester.py
@@ -0,0 +1,35 @@
+import subprocess
+import datetime
+import uuid
+import shutil
+import os
+
+class Tester():
+
+    def __init__(self, commands):
+        self.commands = commands
+        self.log_name = "crash-{}.log".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    def run(self):
+        if not os.path.exists("./tmp"):
+            os.makedirs("./tmp")
+        if not os.path.exists("./passlogs"):
+            os.makedirs("./passlogs")
+        if not os.path.exists("./crashlogs"):
+            os.makedirs("./crashlogs")
+        main_log = open(self.log_name, "a")
+        for command in self.commands:
+            run_id = str(uuid.uuid4())
+            runlog_name = "./tmp/{}.log".format(run_id)
+            runlog = open(runlog_name, "w")
+            retcode = subprocess.call(command, stdout=runlog, shell=True)
+            logdir = "passlogs"
+            if not retcode == 0:
+                logdir = "crashlogs"
+                main_log.write("Command returned non-zero code:\n\t{} - {}\n".format(
+                    " ".join(command), retcode))
+                main_log.write("\tLog available at: ./crashlogs/{}.log\n".format(run_id))
+            runlog.close()
+            shutil.move(runlog_name, "./{}/{}.log".format(logdir, run_id))
+        main_log.close()
+            
